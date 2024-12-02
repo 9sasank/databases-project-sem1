f69e3d240697882f3b8b23e091b39671fda80262
0xjai <115560726+0xjai@users.noreply.github.com>
2024-12-02 12:22:00 -0500

    Update connection.py

--- connection.py
+++ connection.py
@@ -3,17 +3,17 @@ import psycopg2
 def connect_to_db():
     try:
         conn = psycopg2.connect(
-            dbname="cs623_project",   # Replace with your database name
-            user="jaisheelpaladugu",          # Replace with your PostgreSQL username
-            password="080103", # Replace with your PostgreSQL password
+            dbname="cs623_project",   
+            user="jaisheelpaladugu",          
+            password="080103", 
             host="localhost",
-            port="5432"               # Default PostgreSQL port
+            port="5432"               
         )
         print("Connected to the database successfully!")
         conn.close()
     except Exception as e:
         print(f"Error connecting to the database: {e}")
 
-# Test the connection
+
 if __name__ == "__main__":
     connect_to_db()
